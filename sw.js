08d61d5e84f26bb71edecf82cabd03f6f14ed22b
nyh233 <3558539135@qq.com>
2022-07-29 19:39:00 +0800

    Site updated: 2022-07-29 19:39:00

--- sw.js
+++ sw.js
@@ -10,7 +10,7 @@
 
 'use strict';
 
-var precacheConfig = [];
+var precacheConfig = [["/sw-register.js","801aa90b9a1c415087c33acde31bf849"]];
 var cacheName = 'sw-precache-v3--' + (self.registration ? self.registration.scope : '');
 var firstRegister = 1; // 默认1是首次安装SW， 0是SW更新
 
